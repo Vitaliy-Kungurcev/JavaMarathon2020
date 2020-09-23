6165d84b8845004385a8fa1800d7700a07d8a78c
Vitaliy Kungurcev <vital708@mail.ru>
2020-09-23 11:09:06 +0500

    Day 2 complete

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if ((1 <= x) && (x <= 4)) {
+            System.out.println("Малоэтажный дом");
+        } else if ((5 <= x) && (x <= 8)) {
+            System.out.println("Среднеэтажный дом");
+        } else if (9 <= x) {
+            System.out.println("Многоэтажный дом");
+        } else if (x <= 0) {
+            System.out.println("Ошибка ввода");
 
+
+        }
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,25 @@
 package day2;
 
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
 
+        if (x >= 5) {
+            double y = ((pow(x, 2)) - 10) / (x + 7);
+            System.out.println(y);
+        } else if ((-3 < x) && (x < 5)) {
+            double y = (x + 3) * ((pow(x, 2)) - 2);
+            System.out.println(y);
+        } else {
+            double y = 420;
+            System.out.println(y);
+
+        }
     }
 }
+
