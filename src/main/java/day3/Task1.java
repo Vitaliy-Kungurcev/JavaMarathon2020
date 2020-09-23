120136ab9fee8fd5484858a913149b5118b76b14
Vitaliy Kungurcev <vital708@mail.ru>
2020-09-23 12:43:19 +0500

    Day 3 complete

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,53 @@
 package day3;
 
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////
+// ПРОГРАММА ПО УСЛОВИЯМ ЗАДАЧИ РАБОТАЕТ,НО ТЕСТ НЕ ПРОХОДИТ.
+// ПРОШУ ДАТЬ ПОЯСНЕНИЕ,ГДЕ МОЯ ОШИБКА.
+////////////////////////////////////////////////////////////////////////
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String gorod = scanner.nextLine();
+
+            if (gorod.equals("Stop")) {
+                break;
+            }
+
+            switch (gorod) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Англия");
+                    break;
+
+                default:
+
+                    System.out.println("Неизвестная страна");
+
+            }
+
+        }
 
     }
 }
+
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,27 @@
 package day3;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int i = 1;
+        while (i < 5) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                i--;
+                continue;
+
+            }
+
+            System.out.println(a / b);
+            i++;
+        }
     }
 }
