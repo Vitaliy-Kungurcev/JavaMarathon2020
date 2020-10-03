f00290f0c6ebca7764957bbe70e5e41ec22313b4
Vitaliy Kungurcev <vital708@mail.ru>
2020-10-03 22:55:29 +0500

    test

--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,75 +1,80 @@
-package day7;
+    package day7;
 
-public class Task2 {
-    public static void main(String[] args) {
+    import java.util.Random;
 
-        Player player1 = new Player();
-        player1.setStamina(91);
-        Player player2 = new Player();
-        player2.setStamina(92);
-        Player player3 = new Player();
-        player3.setStamina(93);
-        Player player4 = new Player();
-        player4.setStamina(94);
-        Player player5 = new Player();
-        player5.setStamina(95);
-        Player player6 = new Player();
-        player6.setStamina(96);
+    public class Task2 {
+        public static void main(String[] args) {
+            Random random = new Random();
 
+            Player player1 = new Player(91);
 
-        while (player1.getStamina() != Player.MIN_STAMINA) {
-            player1.run();
-        }
+            Player player2 = new Player(95);
+
+            Player player3 = new Player(93);
+
+            Player player4 = new Player(97);
+
+            Player player5 = new Player(98);
+
+            Player player6 = new Player(99);
 
 
-        Player.info();
-    }
-}
-
-class Player {
-
-
-    private int stamina;
-    public static final int MAX_STAMINA = 100;
-    public static final int MIN_STAMINA = 0;
-    static int countPlayer;
-    static String free;
-
-    public void setStamina(int stamina) {
-        this.stamina = stamina;
-    }
-
-    public int getStamina() {
-        return stamina;
-
-    }
-
-    public Player() {
-        if (countPlayer < 6) {
-            Player.countPlayer++;
-
-        }
-    }
-
-    public void run() {
-        stamina--;
-        if (stamina == MIN_STAMINA) {
-            countPlayer--;
-        }
-    }
-
-    public static void info() {
-        if (countPlayer < 6) {
-            if (countPlayer == 5) {
-                free = " свободное место";
-            } else {
-                free = " свободных места";
+            while (player1.getStamina() != Player.getMinStamina()) {
+                player1.run();
             }
 
-            System.out.println("Команды неполные, еще есть " + (6 - countPlayer) + free);
-        } else System.out.println("Мест в командах больше нет");
+
+            Player.info();
+        }
     }
 
+    class Player {
+        private int stamina;
+        private static final int MAX_STAMINA = 100;
+        private static final int MIN_STAMINA = 0;
+        private static int countPlayer;
+        static String free;
 
-}
+        public void setStamina(int stamina) {
+            this.stamina = stamina;
+        }
+
+        public int getStamina() {
+            return stamina;
+        }
+
+        public static int getMinStamina() {
+            return MIN_STAMINA;
+        }
+
+        public Player(int stamina) {
+            this.stamina = stamina;
+            if (countPlayer < 6) {
+                countPlayer++;
+            }
+        }
+
+        public void run() {
+            if (stamina == 0) {
+                return;
+            }
+            stamina--;
+            if (stamina == MIN_STAMINA) {
+                countPlayer--;
+            }
+        }
+
+        public static void info() {
+            if (countPlayer < 6) {
+                if (countPlayer == 5) {
+                    free = " свободное место";
+                } else {
+                    free = " свободных места";
+                }
+                System.out.println("Команды неполные, еще есть " + (6 - countPlayer) + free);
+            } else System.out.println("Мест в командах больше нет");
+        }
+
+
+    }
 
