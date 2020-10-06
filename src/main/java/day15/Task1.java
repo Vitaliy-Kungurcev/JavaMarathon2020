d64865895b837b1574c27b9bd4b4ffb5d886ee9f
Vitaliy Kungurcev <vital708@mail.ru>
2020-10-06 13:53:40 +0500

    day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,24 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File fileRead = new File("D:\\Programs\\JavaMarathon20202\\src\\main\\resources\\shoes.csv");
+        File fileWrite = new File("D:\\Programs\\JavaMarathon20202\\src\\main\\resources\\missing_shoes.txt");
+        PrintWriter pw = new PrintWriter(fileWrite);
+        Scanner sc = new Scanner(fileRead);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] arr = line.split(";");
+            if (Integer.parseInt(arr[2]) == 0) {
+                pw.println(line);
+            }
+        }
+        pw.close();
     }
 }
+
